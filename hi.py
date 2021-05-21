449c1374c62c09f702abbb83a77732abbbd68bc0
ChanJin Chung <cchung@ltu.edu>
2021-05-20 22:55:47 -0400

    displaying big HIs

--- hi.py
+++ hi.py
@@ -1,8 +1,11 @@
 # Simple program to introduce Git and GitHub
 import sys
+from big_HI import *
+
 if len(sys.argv) == 1: # no command line arg
     n = 4
 else:
     n = int(sys.argv[1])
 for _ in range(n):
-    print('hi')
+    #print('hi')
+    display_big_HI()
