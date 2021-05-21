c6e75d43ad636b29861131a59c6bb7c0bf10453a
ChanJin Chung <cchung@ltu.edu>
2021-05-20 22:30:59 -0400

    command line arg handling added

--- hi.py
+++ hi.py
@@ -1,3 +1,8 @@
 # Simple program to introduce Git and GitHub
-for _ in range(4):
+import sys
+if len(sys.argv) == 1: # no command line arg
+    n = 4
+else:
+    n = int(sys.argv[1])
+for _ in range(n):
     print('hi')
